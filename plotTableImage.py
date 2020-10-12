a76bf721b21ea92a7ede5efe7aac05d439acc1d3
Shiwei Ye <shiwei.ye@datavisor.com>
2020-10-12 23:01:38 +0800

    a script to plot all table images.

---
+++ plotTableImage.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import six
+from common import updateCityTable, updateAllTableImage
+updateCityTable()
+updateAllTableImage()
