298abc283fa0f5b0acbba9d15ea4bee20cc882f2
Shiwei Ye <shiwei.ye@datavisor.com>
2020-11-28 18:49:33 +0800

    add try in main plot city.

--- updatePlot.py
+++ updatePlot.py
@@ -10,15 +10,19 @@ os.system('rm fig/allcity/*')
 #cityList = ['北京']
 #cityList = ['深圳']
 #cityList = ['宁波']
+#cityList=['天津']
 data = {}
 res = {}
 districtRes = {}
 for city in cityList:
     print(city)
-    df = read(city)
-    data[city] = df
-    res[city] = plotCity(df, city)
-    districtRes[city] = plotAllDistrict(df, city)
+    try:
+        df = read(city)
+        data[city] = df
+        res[city] = plotCity(df, city)
+        districtRes[city] = plotAllDistrict(df, city)
+    except Exception as e:
+        print(e)
 
 for city in cityList:
     makeTable(districtRes[city], '城区', city)
