cf64f35344d754b09522b2e75a2f4f6f4d8556ef
Shiwei Ye <yeshiwei.math@gmail.com>
2021-04-17 19:23:00 +0800

    Change rsync target server.

--- rsync.sh
+++ rsync.sh
@@ -1,1 +1,1 @@
-rsync -avz ubuntu@qq.yeshiwei.com:/home/ubuntu/LianjiaSpider/data/ data/ -e "ssh -i ~/.ssh/tencent_wujing.key"
+rsync -avz root@ali2.yeshiwei.com:/root/code/LianJiaSpider/data/ data/ -e "ssh -i ~/.ssh/ali2.pem"
